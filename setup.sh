d7c11f390b5af74673957543d28af69145b66710
MeisamKargar <meisamcho2@gmail.com>
2024-11-26 14:30:07 +0330

    First commit

---
+++ setup.sh
@@ -0,0 +1,10 @@
+###Sets up python packages in for devcontainer.json
+
+#create a virtualenv
+virtualenv ~/.venv
+#source virtualenv
+source ~/.venv/bin/activate
+#append it to bash so every shell launches with it 
+echo 'source ~/.venv/bin/activate' >> ~/.bashrc
+#install all software 
+make install
